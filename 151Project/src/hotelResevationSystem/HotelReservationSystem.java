aaf3f5318e7007d217aa03c459e92774d787e4a0
Elizabeth Wong <39285862+elizabeth-wong-ab@users.noreply.github.com>
2018-11-27 12:21:59 -0800

    Create HotelReservationSystem.java

---
+++ 151Project/src/hotelResevationSystem/HotelReservationSystem.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class HotelReservationSystem {
+	
+	private Room[] luxoryRooms;
+	private Room[] economicRooms;
+	private HashMap<User, Room> accounts;
+	
+	public HotelReservationSystem(int luxory, int economic)
+	{
+		luxoryRooms = new Room[luxory];
+		for(int i = 0; i < luxory ; i++)
+		{
+			luxoryRooms[i] = new Luxory();
+		}
+	}
+
+}
