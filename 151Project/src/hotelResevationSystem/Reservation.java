10875a65882f2e199728d696be35ac540bf9d539
Christian <ccastro715@gmail.com>
2018-12-07 22:16:27 -0800

    Changed variable type and added method

--- 151Project/src/hotelResevationSystem/Reservation.java
+++ 151Project/src/hotelResevationSystem/Reservation.java
@@ -2,28 +2,30 @@ package hotelResevationSystem;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
  * Reservation object holds data on a specific reservation made and 
- * @author Carl Shefcik
+ * @author Carl Shefcik, Christian Castro
  *
  */
 public class Reservation {
 
 	private String reservationID;
-	//probably replace ; with a time interval class
-	private Date startDate;
-	private Date endDate;
+	private LocalDate startDate;
+	private LocalDate endDate;
 	private String userID;
 	private String roomNumber;
 	
 	public Reservation(String reservationID, String startDate, String endDate, String userID, String roomNumber) {
 		this.reservationID = reservationID;
 		try {
-			this.startDate = new SimpleDateFormat("dd/MM/yyyy").parse(startDate);
-			this.endDate = new SimpleDateFormat("dd/MM/yyyy").parse(endDate);
-		} catch (ParseException e) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+			this.startDate = LocalDate.parse(startDate, formatter);
+			this.endDate = LocalDate.parse(endDate, formatter);
+		} catch (Exception e) {
 //			e.printStackTrace();
 		};
 		this.userID = userID;
@@ -34,5 +36,17 @@ public class Reservation {
 	public String getReservationID() { return reservationID; }
 	public String getUserID() { return userID; }
 	public String getRoom() { return roomNumber; }
+	public LocalDate getStartDate() {return startDate;}
+	public LocalDate getEndDate() {return endDate;}
+	
+	/**
+	 * Constructs string containing details of reservation
+	 * @return details reservation info
+	 */
+	public String resDetails() {
+		String details="Reservation ID: "+reservationID+" Guest ID: "+userID+" Room Number: "+roomNumber
+				+ " Start Date: "+startDate.toString()+" End Date: "+endDate.toString();
+		return details;
+	}
 	
 }
