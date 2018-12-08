551771c32b97eddf046f32d51c6cbee961e7ee7a
Elizabeth Wong <39285862+elizabeth-wong-ab@users.noreply.github.com>
2018-12-08 13:58:44 -0800

    Update ViewReservations.java
    Repaint method won't call

--- 151Project/src/hotelResevationSystem/ViewReservations.java
+++ 151Project/src/hotelResevationSystem/ViewReservations.java
@@ -1,3 +1,4 @@
+/**
 package hotelResevationSystem;
 
 import javax.swing.BoxLayout;
@@ -21,6 +22,57 @@ public class ViewReservations extends ViewContent {
 			changeView("Guest Menu");
 		});
 		// TODO Auto-generated constructor stub
+	}
+
+}
+*/
+package hotelResevationSystem;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+
+public class ViewReservations extends ViewContent {
+
+	private User currentUser;
+	
+	public ViewReservations(HotelReservationSystem hrs) {
+		super(hrs);
+		currentUser = hrs.getCurrentUser();
+		
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		JTextField tf = new JTextField();
+		tf.setText("View Reservations");
+		add(tf);
+
+		hrs.sop("Repainted");
+		if(currentUser == null)
+		{
+			JLabel error = new JLabel(" Sorry, error ");
+			add(error);
+		}
+		else
+		{
+			JLabel yay = new JLabel(" Cool ");
+			add(yay);
+		}
+		
+		JButton menuButton = new JButton("Guest Menu");
+		add(menuButton);
+		
+		
+		menuButton.addActionListener(e ->{
+			changeView("Guest Menu");
+		});
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		currentUser = hrs.getCurrentUser();
+		hrs.sop("Current User: " + currentUser.getUsername());
+		super.stateChanged(e);
 	}
 
 }
